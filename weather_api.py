8889f569c0659529ab66b7cd4196a275cc5e35cb
Cathryn <cllytle5447@eagle.fgcu.edu>
2025-11-19 19:32:52 -0600

    added exceptions if api crashes

--- weather_api.py
+++ weather_api.py
@@ -10,22 +10,36 @@ api_key = "22201b829f3b46838d2163913251711"
 import requests
 
 #function for the current weather data
+#also crashes program if WeatherAPI is down
 def get_weather(city):
-   url = c_api_url + "?key=" + api_key + "&q=" + city
-   response = requests.get(url)
-   data = response.json()
-   return data
+    try:
+        url = c_api_url + "?key=" + api_key + "&q=" + city
+        response = requests.get(url)
+        response.raise_for_status()
+        data = response.json()
+        return data
+    except requests.exceptions.RequestException:
+        return {"error": "Network error while getting weather."}
 
 #function for the forecasted weather data
+#crashes program if WeatherAPI is down
 def forecast(city):
-   url = d_api_url + "?key=" + api_key + "&q=" + city + "&days=1&aqi=no&alerts=no"
-   response = requests.get(url)
-   data = response.json()
-   return data
+    try:
+       url = d_api_url + "?key=" + api_key + "&q=" + city + "&days=1&aqi=no&alerts=no"
+       response = requests.get(url)
+       response.raise_for_status()
+       data = response.json()
+       return data
+    except requests.exceptions.RequestException:
+        return {"error": "Network error while getting weather."}
 
 #function that pulls the sunrise and sunset data
 def astronomical(city):
-   url = a_api_url + "?key=" + api_key + "&q=" + city
-   response = requests.get(url)
-   data = response.json()
-   return data
+    try:
+       url = a_api_url + "?key=" + api_key + "&q=" + city
+       response = requests.get(url)
+       response.raise_for_status()
+       data = response.json()
+       return data
+    except requests.exceptions.RequestException:
+        return {"error": "Network error while getting weather."}
